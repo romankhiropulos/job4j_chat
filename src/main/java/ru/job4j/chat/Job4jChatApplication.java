7e430a03599557989164afa8de921d27112825ba
romankhiropulos <romankhiropulos@gmail.com>
2021-12-19 16:37:17 +0300

    add liquibase properties with using java-bean in Main class, improve version of library liquibase in pom.xml. All working on my pc

--- src/main/java/ru/job4j/chat/Job4jChatApplication.java
+++ src/main/java/ru/job4j/chat/Job4jChatApplication.java
@@ -1,15 +1,26 @@
 package ru.job4j.chat;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Job4jChatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Job4jChatApplication.class, args);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 
     @Bean
